9b90a6fe2253a34b23dde112cbe3cb63e7955468
Shahryar70 <mshahryar408@gmail.com>
2025-05-23 14:09:22 +0500

    Add project files.

---
+++ C#Learning/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q2. Write a program that converts temperature from Celsius to Fahrenheit.
+            //double celcius = 100;
+            //double farheniet = celcius * 9/5 + 32;
+            //Console.WriteLine(farheniet);
+
+            //Q2. Ask the user to enter principal, rate of interest, and time.Then calculate Simple Interest using:
+            //Console.Write("Enter principal ammount");
+            //string principalValue = Console.ReadLine();
+            //double principal = Convert.ToDouble(principalValue);
+            //Console.Write("Enter rate of interest");
+            //string rateValue = Console.ReadLine();
+            //double interest = Convert.ToDouble(rateValue);
+            //Console.Write("Enter time in years");
+            //string yearValue = Console.ReadLine();
+            //double year = Convert.ToDouble(yearValue);
+            //double simpleInterest = (principal * interest * year) / 100;
+            //Console.WriteLine("Simple Interet is:" + simpleInterest);
+
+            // Q3. Write a program that takes two numbers and swaps their values using a third variable.
+            //int a = 10;
+            //int b = 20;
+            //Console.WriteLine("Before swapping: a = " + a + " and b = " + b);
+            //int c;
+            //c = b;
+            //b = a;
+            //a = c;
+            //Console.WriteLine("After swapping: a = " + a + " and b = " + b);
+
+            //Ask the user to enter marks for 5 subjects.Calculate:
+            //Console.Write("Enter marks of subject 1; ");
+            //double marks = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter marks of subject 2; ");
+            //double marks2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter marks of subject 3; ");
+            //double marks3 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter marks of subject 4; ");
+            //double marks4 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter marks of subject 5; ");
+            //double marks5 = Convert.ToDouble(Console.ReadLine());
+            //double totalMarks = marks + marks2 + marks3 + marks4 + marks5;
+            //double perecentage = (totalMarks / 500) * 100;
+            //Console.WriteLine("Total marks: " + totalMarks);
+            //Console.WriteLine("Percentage: " + perecentage);
+
+            //🔹 1.Simple Calculator
+            //Console.Write("Enter first number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter the second number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter the operator (+, - , *, /, %): ");
+            //char op = Convert.ToChar(Console.ReadLine());
+            //double result = 0;
+            //switch(op)
+            //{
+            //    case '+':
+            //        result = num1 + num2;
+            //        break;
+            //    case '-':
+            //        result = num1 - num2;
+            //        break;
+            //    case '*':
+            //        result = num1 * num2;
+            //        break;
+            //    case '/':
+            //        result = num1 / num2;
+            //        break;
+            //    case '%':
+            //        result = num1 % num2;
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid operator");
+            //        break;
+            //}
+            //Console.WriteLine("Result: " + result);
+
+            //🔹 2.Check Even or Odd
+            //Console.Write("Enter a number want to check if number is even of odd: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("The number is even.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("the number id odd");
+            //}
+            //🔹 3.Find the Largest of 3 Numbers
+            //Console.Write("Enter first number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter second number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter third number: ");
+            //double num3 = Convert.ToDouble(Console.ReadLine());
+
+            //if(num1>num2 & num1 >num3)
+            //{
+            //    Console.WriteLine($"{num1} is greater");
+            //}
+            //else if(num2>num1 & num2 >num3)
+            //{
+            //    Console.WriteLine($"{num2} is greater");
+            //}
+            //else if(num3>num1 & num3>num1)
+            //{
+            //    Console.WriteLine($"{num3} is greater");
+            //}
+
+            //🔹 4.Check if a Number is Positive, Negative, or Zero
+            //int nuumber = 1;
+            //if (nuumber > 0)
+            //{
+            //    Console.WriteLine("The number is positive");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("the number is negative");
+            //}
+            //🔹 5.Swap Two Numbers(Without Third Variable)
+            Console.ReadLine();
+        }
+    }
+}
